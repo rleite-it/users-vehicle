fdb30abc6e21716c025dafc773a8f3456cc78338
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-02-04 16:10:06 +0000

    Update store.js file

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { filterSlice } from "components/Filter/filterSlice";
-import appSlice from "src/app/appSlice";
+import filterReducer from "components/Filter/filterSlice";
+import listReducer from "components/List/listSlice";
 
 export const store = configureStore({
 	reducer: {
-		app: appSlice,
-		filter: filterSlice,
+		filter: filterReducer,
+		list: listReducer,
 	},
 });
